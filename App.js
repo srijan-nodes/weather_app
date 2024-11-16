55ca8082bb7d8ba6dafd3ed6893029891e9aefaa
srijan-nodes <144047336+srijan-nodes@users.noreply.github.com>
2024-11-17 00:33:38 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to the Weather App</h1>
+      <ul>
+        <li><a href="home.html" target="_blank">Home</a></li>
+        <li><a href="location.html" target="_blank">Change Location</a></li>
+        <li><a href="current.html" target="_blank">Current Weather</a></li>
+        <li><a href="forecast.html" target="_blank">15-Day Forecast</a></li>
+        <li><a href="about.html" target="_blank">About Weather Components</a></li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
